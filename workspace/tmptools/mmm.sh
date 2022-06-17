8a3b26ee1d942a1bd6f38bd087d5175ebb200998
hohno-46466 <hohno.46466@gmail.com>
2022-06-17 09:12:11 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

---
+++ workspace/tmptools/mmm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mount
+echo
+mount -t drvfs h: /mnt/usb1
+mount -t drvfs i: /mnt/usb2
+mount -t drvfs j: /mnt/usb3
+mount -t drvfs k: /mnt/usb4
+mount -t drvfs l: /mnt/usb5
+#mount -t drvfs m: /mnt/usb6
+#mount -t drvfs n: /mnt/usb7
+#mount -t drvfs o: /mnt/usb8
+mount
+echo
---
+++ workspace/tmptools/xxx.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /home/hohno/GitHub/dZone--PT4A--USBstorage/USB256GB-20220612 || exit 99
+
+tar --exclude="kemotw*.pdf" --exclude=".dummy4github" -cf - . | ( cd /mnt/s; tar xmf - ); diff -r -w -q . /mnt/s  2>&1 | grep  '^Only' | egrep -v '/Tokyo2020/|/amedas/' | cat -n
+
+# for x in 1 2 3 4 5 6 7 8; do date; echo $x; tar --exclude="kemotw*.pdf" --exclude=".dummy4github" -cf - . | ( cd /mnt/usb$x; tar xmf - ); diff -r -w -q . /mnt/usb$x  2>&1 | grep  '^Only' | egrep -v '/Tokyo2020/|/amedas/' | cat -n; echo; echo; done; date
